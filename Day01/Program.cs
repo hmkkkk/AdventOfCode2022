a1c3aa4f5bb5abee9994986117499323bc97cab5
Pawel <pawel.j.kroczak@gmail.com>
2022-12-01 17:03:10 +0100

    added solution and gitignore files; Day 01 solution upload

---
+++ Day01/Program.cs
@@ -0,0 +1,27 @@
+﻿string path = Path.Combine(Environment.CurrentDirectory, "input.txt");
+
+var data = File.ReadAllLines(path);
+
+List<int> topThree = new List<int> { 0, 0, 0 };
+int currentVal = 0;
+
+foreach (var line in data)
+{
+    if (String.IsNullOrEmpty(line))
+    {
+        if (topThree.Any(x => x < currentVal))
+        {
+            int indexOfMin = topThree.IndexOf(topThree.Min());
+            topThree[indexOfMin] = currentVal;
+        }
+
+        currentVal = 0;
+        continue;
+    }
+
+    Int32.TryParse(line, out int lineVal);
+    currentVal += lineVal;
+}
+
+
+Console.WriteLine(topThree.Sum());
