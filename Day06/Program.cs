5811245e730f8effaf55fdfd0afcbcf830e605ee
Pawel <pawel.j.kroczak@gmail.com>
2022-12-06 17:33:12 +0100

    Day 06 solution upload

---
+++ Day06/Program.cs
@@ -0,0 +1,16 @@
+﻿string path = Path.Combine(Environment.CurrentDirectory, "input.txt");
+
+var data = File.ReadAllText(path);
+
+int messageLength = 14;
+
+for (int i = 0; i < data.Length - messageLength; i++)
+{
+    string message = data.Substring(i, messageLength);
+
+    if (message.Distinct().Count() == message.Length) 
+    {
+        Console.WriteLine(i + messageLength);
+        break;
+    }
+}
